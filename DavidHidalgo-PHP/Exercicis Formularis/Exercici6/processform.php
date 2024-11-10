649479755909547deaec010bb5b4dc22c1802d44
DavidHS123 <deividhs123@gmail.com>
2024-11-10 17:30:52 +0100

    Update processform.php

--- DavidHidalgo-PHP/Exercicis Formularis/Exercici6/processform.php
+++ DavidHidalgo-PHP/Exercicis Formularis/Exercici6/processform.php
@@ -7,13 +7,13 @@
 </head>
 <body>
     <h1>Recopilacio de les dades</h1>
-    <p>El nom és: <?php echo $_POST['name']; ?></p>
+    <p>El nom és: <?php echo $_POST['nom']; ?></p>
     <p>El email és: <?php echo $_POST['email']; ?></p>
     <p>El telefon és: <?php echo $_POST['telefon']; ?></p>
     <p>GPS: <?php if (isset($_POST['gps'])) echo 'Si'; else echo 'No'; ?></p>
     <p>Mapa: <?php  if (isset($_POST['mapa'])) echo 'Si'; else echo 'No'; ?> </p>
     <p>Seguiment: <?php if (isset($_POST['seguiment'])) echo 'Si'; else echo 'No'; ?> </p>
-    <p>Adreça: <?php echo $_POST['adreça']; ?></p>
+    <p>Adreça: <?php echo $_POST['adreça']; ?></p>
     <p>Descripcio: <?php echo $_POST['descripcio']; ?></p>
 </body>
 </html>
