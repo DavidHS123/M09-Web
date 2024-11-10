1bc441c7bff276b634c2bd996bae29434fb5ede0
DavidHS123 <deividhs123@gmail.com>
2024-11-10 17:26:26 +0100

    Exercici6

---
+++ DavidHidalgo-PHP/Exercicis Formularis/Exercici6/main.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dades Formulari</title>
+</head>
+<body>
+    <h1>Dades de l'avís</h1>
+    <form action="processform.php" method="POST">
+        <label for="gps">Recollir la localitzacio GPS </label>
+            <input type="checkbox" name="gps" id="gps"> <br>
+        <label for="mapa">Marcar un punt en el mapa </label>
+            <input type="checkbox" name="mapa" id="mapa"> <br>
+        <label for="seguiment"> Voleu rebre un seguiment? </label>
+            <select id="seguiment" name="seguiment" required>
+                <option value="" disabled selected>Selecciona una opció</option>
+                <option value="si">Sí</option>
+                <option value="no">No</option>
+            </select><br>
+        <label for="Adreça">Adreça de l'avís</label>
+            <input type="text" name="Adreça" id="adreça" required><br>
+        <label for="descripcio">Descripcio de l'avís:</label>
+            <input type="text" name="descripcio" id="descripcio" required><br>
+        <label for="Nom">Nom</label>
+            <input type="text" name="Nom" id="nom" required><br>
+        <label for="Cognoms">Cognoms</label>
+            <input type="text" name="Cognoms" id="cognoms" required><br>
+        <label for="telefon">Telefon</label>
+            <input type="tel" name="telefon" id="telefon"><br>
+        <label for="email">Correu electrònic</label>
+            <input type="email" name="email" id="email"><br>
+        <label for="email2">Confirmar correu electrònic</label>
+            <input type="email" name="email2" id="email2"><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ DavidHidalgo-PHP/Exercicis Formularis/Exercici6/processform.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recopilacio de les dades</title>
+</head>
+<body>
+    <h1>Recopilacio de les dades</h1>
+    <p>El nom és: <?php echo $_POST['name']; ?></p>
+    <p>El email és: <?php echo $_POST['email']; ?></p>
+    <p>El telefon és: <?php echo $_POST['telefon']; ?></p>
+    <p>GPS: <?php if (isset($_POST['gps'])) echo 'Si'; else echo 'No'; ?></p>
+    <p>Mapa: <?php  if (isset($_POST['mapa'])) echo 'Si'; else echo 'No'; ?> </p>
+    <p>Seguiment: <?php if (isset($_POST['seguiment'])) echo 'Si'; else echo 'No'; ?> </p>
+    <p>Adreça: <?php echo $_POST['adreça']; ?></p>
+    <p>Descripcio: <?php echo $_POST['descripcio']; ?></p>
+</body>
+</html>
